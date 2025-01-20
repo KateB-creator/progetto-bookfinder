791329944be72a44939c8e4f5ad6c0a780faa9fc
Katiuscia <katiusciabalia@gmail.com>
2025-01-20 12:29:26 +0100

    modifiche bookfinder

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 const path = require('path');
 const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 // Carica il file .env
 dotenv.config();
 
 module.exports = {
-  entry: './src/js/app.js', // Percorso corretto per il file di ingresso
+  entry: './src/js/app.js',
   output: {
-    filename: 'bundle.js', // Nome del file generato (niente './dist/')
-    path: path.resolve(__dirname, 'dist'), // Cartella di output
-    clean: true, // Pulisce la cartella dist prima di ogni build
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Pulisce automaticamente la cartella dist
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // Estrae il CSS in un file separato
+          'css-loader', // Interpreta i file CSS
+        ],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'img/[name].[hash].[ext]', // Copia le immagini nella cartella dist/img
-            },
-          },
-        ],
+        test: /\.(jpg|jpeg|png|gif|svg)$/i,
+        type: 'asset/resource', // Sostituisce file-loader in Webpack 5
+        generator: {
+          filename: 'img/[name].[hash][ext]', // Salva le immagini nella cartella img con un nome unico
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html', // File HTML di origine
       inject: 'body',
-      favicon: './src/img/favicon.svg',
+      favicon: './src/img/favicon.svg', // Specifica la favicon
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Nome del file CSS generato
     }),
   ],
   devServer: {
@@ -43,8 +46,8 @@ module.exports = {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
-    port: 3000, // Cambia porta
+    port: 3000, // Porta del server locale
+    open: true, // Apre automaticamente il browser
   },
-  mode: 'development',
+  mode: 'development', // Imposta la modalità di sviluppo
 };
-
